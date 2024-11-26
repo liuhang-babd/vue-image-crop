d30bf6f34e4de3ca15fcca85e73050a94795451e
liuhang <liuhangbbc@sina.com>
2024-11-26 16:46:32 +0800

    INIT 初始化图片截图项目

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <image-crop :options="options"></image-crop>
+  </div>
+</template>
+
+<script>
+import ImageCrop from './VueImageCrop.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ImageCrop
+  },
+  data() {
+    return {
+      options: {
+        base: {
+          width: 300,
+          height: 300
+        },
+      }
+    }
+  },
+  mounted() {
+    
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/VueImageCrop.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="frame">
+    <div class="container" :style="">
+      <!-- helloworld -->
+    </div>
+  </div>
+</template>
+    return {
+
+<script>
+export default {
+  props: {
+    options: {
+      type: Object,
+      default: null
+    }
+  },
+  data() {
+    return {
+      base: {
+        width: 300,
+        height: 300
+      }
+    }
+  },
+  created() {
+    this.updateBaseFromOptions();
+  },
+  watch: {
+    options: {
+      handler(newVal, oldVal) {
+        console.log('监听到 options 改变了', newVal, oldVal)
+        this.updateBaseFromOptions();
+      },
+      deep: true
+    }
+  },
+  methods: {
+    updateBaseFromOptions() {
+      if (this.options && this.options.base) {
+        this.base = { ...this.base, ...this.options.base };
+      }
+    }
+  }
+}
+</script>
+
+<style>
+.frame {
+  background-color: darkgrey;
+  display: flex;
+  /* Safari */
+  display: -webkit-flex;
+  justify-content: center;
+}
+
+.container {
+  background-color: #ffc;
+  width: 300px;
+  height: 300px;
+}
+</style>
